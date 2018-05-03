52b8fd27c5a4324c52dcec7e7cb7dc48b308bcb6
Aidan San <muakasan@gmail.com>
2018-05-02 21:20:12 -0500

    updated filereading

--- chunker_io.py
+++ chunker_io.py
@@ -1,19 +1,21 @@
-def read_data_from_file(filename):
+def read_data_from_file(filename, sep=' '):
     data = []
     sent_counter = 0
     with open(filename) as infile:
         lines = infile.readlines()
         sent_t = ([], [])
         for line in lines:
-            word, chunk_tag = line.strip().split()
-            if word == '.':
-                if sent_t[0]:
-                    data.append(sent_t)
-                    sent_t = ([], [])
-                    sent_counter += 1
-            else:
-                sent_t[0].append(word)
-                sent_t[1].append(chunk_tag)
+            lsplit = line.strip().split(sep)
+            if len(lsplit) == 2:
+                word, chunk_tag = lsplit
+                if word == '.':
+                    if sent_t[0]:
+                        data.append(sent_t)
+                        sent_t = ([], [])
+                        sent_counter += 1
+                else:
+                    sent_t[0].append(word)
+                    sent_t[1].append(chunk_tag)
     return data
 
 def read_data_from_dir(dir):
